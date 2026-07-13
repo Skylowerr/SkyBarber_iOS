e7e84abca51982df2e778d6ba29760f5b51341e9
Skylowerr <gokceemirhan23@gmail.com>
2026-07-13 23:56:03 +0300

    Initial Commit

---
+++ SkyBarber_iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SkyBarber_iOS
+//
+//  Created by Emirhan Gökçe on 13.07.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
